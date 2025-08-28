0469f5b35c2e0c76c50dea3480f0a7f9947d3562
CriticalRange <muhammedkosgen0608112@gmail.com>
2025-08-28 15:23:16 +0300

    first commit

---
+++ src/client/java/com/criticalrange/VulkanModExtraClient.java
@@ -0,0 +1,10 @@
+package com.criticalrange;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VulkanModExtraClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
